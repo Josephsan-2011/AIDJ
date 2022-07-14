408d598e8cb7b1f178ffe42a18080357fd18b483
Josephsan-2011 <83101904+Josephsan-2011@users.noreply.github.com>
2022-07-13 19:42:55 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
  leftWristX = 0
  leftWristY = 0
  rightWristX = 0
- leftWristY = 0
+ rightWristY = 0
 
 
 
@@ -18,19 +18,47 @@
      posenet = ml5.poseNet(video, model_loaded)
      posenet.on("pose", got_poses)
  }
- function draw() {
-    image(video, 0, 0, 500, 500)
 
-    fill("blue")
-    stroke("blue")
-    if (scoreLeftWrist>0.2) {
-    circle(leftWristX, leftWristY, 25)
-    m1 = Number(leftWristY)
-    remove_decmils = floor(m1)
-    volume = remove_decmils / 500
-    document.getElementById("volume").innerHTML = "Volume =" + volume
-    song.setVolume(volume)
-}}
+ function draw() {
+     image(video, 0, 0, 500, 500)
+
+     fill("blue")
+     stroke("blue")
+     if (rightWristY>0 && rightWristY<=100) {
+        document.getElementById("speed").innerHTML = "Speed =0.5x"
+        song.rate(0.5)
+        
+     }
+     if (rightWristY>100 && rightWristY<=200) {
+        document.getElementById("speed").innerHTML = "Speed =1.0x" 
+        song.rate(1.0)
+        
+     }
+     if (rightWristY>200 && rightWristY<=300) {
+        document.getElementById("speed").innerHTML = "Speed =1.5x" 
+        song.rate(1.5)
+        
+     }
+     if (rightWristY>300 && rightWristY<=400) {
+        document.getElementById("speed").innerHTML = "Speed =2.0x" 
+        song.rate(2.0)
+        
+     }
+     if (rightWristY>400 && rightWristY<=500) {
+        document.getElementById("speed").innerHTML = "Speed =2.5x" 
+        song.rate(2.5)
+        
+     }
+     if (scoreLeftWrist > 0.2) {
+         circle(leftWristX, leftWristY, 25)
+         m1 = Number(leftWristY)
+         remove_decmils = floor(m1)
+         volume = remove_decmils / 500
+         document.getElementById("volume").innerHTML = "Volume =" + volume
+         song.setVolume(volume)
+     }
+ }
+
  function play() {
      song.play()
      song.setVolume(0.5)
@@ -44,13 +72,13 @@
  function got_poses(results) {
      if (results.length > 0) {
          console.log(results)
-         scoreLeftWrist = reults[0].pose.keypoints[9].score
+         scoreLeftWrist = results[0].pose.keypoints[9].score
          console.log(scoreLeftWrist)
          leftWristX = results[0].pose.leftWrist.x
          leftWristY = results[0].pose.leftWrist.y
          rightWristX = results[0].pose.rightWrist.x
          rightWristY = results[0].pose.rightWrist.y
          console.log(leftWristX, leftWristY)
-         console.log(rightWristX, leftWristY)
+         console.log(rightWristX, rightWristY)
      }
  }
